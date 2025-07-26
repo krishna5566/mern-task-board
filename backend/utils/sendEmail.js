dea909bfb63c5d6e3c3b387301ee3b70ab082b49
krishna miroliya <krishnamiroliya05@gmail.com>
2025-07-26 11:25:19 +0530

    env example

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,9 +3,9 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,         // 👈 Ethereal or other host
-    port: process.env.EMAIL_PORT,         // 587 for Ethereal
-    secure: false,                        // false for TLS (Ethereal)
+    host: process.env.EMAIL_HOST,      
+    port: process.env.EMAIL_PORT,    
+    secure: false,                      
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
